b7deafd1cfb912837287f9566577e37ed5a98626
BO150 <2658275439@qq.com>
2023-07-23 14:29:30 +0800

    第一次使用GitHub来管理代码，版本号003

--- src/main/java/com/example/SpringcloudServer1Application.java
+++ src/main/java/com/example/SpringcloudServer1Application.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * Eureka注册中心启动类
+ * Eureka注册中心
  */
 @SpringBootApplication
 @EnableEurekaServer
